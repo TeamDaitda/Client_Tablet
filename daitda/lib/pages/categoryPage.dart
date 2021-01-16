91a0d99defe33f9e76339331f3c2f670394d3598
Dev-Beom <66074802+Dev-Beom@users.noreply.github.com>
2021-01-16 17:49:40 +0900

    Update Page

--- daitda/lib/pages/categoryPage.dart
+++ daitda/lib/pages/categoryPage.dart
@@ -12,79 +12,288 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _buildHeightSizedBox(50),
+            _buildProcessWidget(),
+            _buildHeightSizedBox(50),
+            _buildBody("어떤 곳에 기부하고 싶으세요?", context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProcessWidget() {
+    return Column(
+      children: [
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Choose the category",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+            _buildCircle(inputColor: Color(0xff9470ac)),
+            _buildLine(inputColor: Color(0xff9470ac)),
+            _buildCircle(inputColor: Color(0xff9470ac)),
+            _buildLine(inputColor: Color(0xffc4c4c4)),
+            _buildCircle(inputColor: Color(0xffc4c4c4)),
+            _buildLine(inputColor: Color(0xffc4c4c4)),
+            _buildCircle(inputColor: Color(0xffc4c4c4)),
+            _buildLine(inputColor: Color(0xffc4c4c4)),
+            _buildCircle(inputColor: Color(0xffc4c4c4)),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildWidthSizedBox(175),
+            _buildText(inputText: "정보입력"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "카테고리 선택"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "결제"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "촬영"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "기부완료"),
+            _buildWidthSizedBox(175),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLine({Color inputColor}) {
+    return Container(
+      height: 1,
+      width: 150,
+      color: inputColor,
+    );
+  }
+
+  Widget _buildCircle({Color inputColor}) {
+    return Container(
+      width: 22,
+      height: 22,
+      decoration: BoxDecoration(
+        color: inputColor,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+
+  Widget _buildText({String inputText}) {
+    return Container(
+      width: 80,
+      child: Text(
+        inputText,
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _buildWidthSizedBox(double _width) {
+    return SizedBox(
+      width: _width,
+    );
+  }
+
+  Widget _buildHeightSizedBox(double _height) {
+    return SizedBox(
+      height: _height,
+    );
+  }
+
+  Widget _buildBody(String inputTitle, BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: 50,
+          height: 50,
+          child: FlatButton(
+            color: Color(0xfff0f0f3),
+            onPressed: () {},
+            child: Icon(Icons.keyboard_arrow_left),
+          ),
+        ),
+        Container(
+          // color: Color(0xfff6f5f5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                inputTitle,
+                style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.start,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            new CupertinoButton(
-              onPressed: () => {},
-              color: Colors.blue,
-              borderRadius: new BorderRadius.circular(30.0),
-              child: Container(
-                height: 100,
-                width: 300,
-                child: Center(
-                  child: new Text(
-                    "Category 1",
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(color: Colors.white),
+              Container(
+                color: Color(0xfff6f5f5),
+                width: 1000,
+                height: 450,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    _buildCard(
+                        title: "Pasicm",
+                        img: AssetImage('images/ps.png'),
+                        context: context),
+                    _buildCard(
+                        title: "Pollution",
+                        img: AssetImage('images/pl.png'),
+                        context: context),
+                    _buildCard(
+                        title: "Global Warming",
+                        img: AssetImage('images/gw.png'),
+                        context: context),
+                    _buildCard(
+                        title: "Pasicm",
+                        img: AssetImage('images/ps.png'),
+                        context: context),
+                    _buildCard(
+                        title: "Pollution",
+                        img: AssetImage('images/pl.png'),
+                        context: context),
+                    _buildCard(
+                        title: "Global Warming",
+                        img: AssetImage('images/gw.png'),
+                        context: context),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 50,
+          height: 50,
+          child: FlatButton(
+            color: Color(0xfff0f0f3),
+            onPressed: () {
+              Navigator.pushNamed(context, '/paymentPage');
+            },
+            child: Icon(Icons.keyboard_arrow_right),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCard({String title, AssetImage img, BuildContext context}) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // IconButton(
+            //   icon: Icon(Icons.check_box_outline_blank_rounded),
+            //   onPressed: () {},
+            // ),
+            Container(
+              width: 200,
+              height: 300,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                elevation: 3,
+                child: InkWell(
+                  onTap: () {},
+                  child: Image(
+                    image: img,
                   ),
                 ),
               ),
             ),
-            SizedBox(
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
               height: 20,
-            ),
-            new CupertinoButton(
-              onPressed: () => {},
-              color: Colors.green,
-              borderRadius: new BorderRadius.circular(30.0),
-              child: Container(
-                height: 100,
-                width: 300,
-                child: Center(
-                  child: new Text(
-                    "Category 2",
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(color: Colors.white),
-                  ),
+              child: FlatButton(
+                onPressed: () {
+                  _settingModalBottomSheet(context, title, img);
+                },
+                child: Text(
+                  "Learn More",
+                  style: TextStyle(color: Colors.white),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            new CupertinoButton(
-              onPressed: () => {},
-              color: Colors.orange,
-              borderRadius: new BorderRadius.circular(30.0),
-              child: Container(
-                height: 100,
-                width: 300,
-                child: Center(
-                  child: new Text(
-                    "Category 3",
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(color: Colors.white),
-                  ),
-                ),
+                color: Color(0xffA7A7A7),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _settingModalBottomSheet(context, title, img) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        builder: (context) {
+          return DraggableScrollableSheet(
+            initialChildSize: 0.6,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return SingleChildScrollView(
+                controller: scrollController,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(10),
+                      topRight: const Radius.circular(10),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text("X"),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            title + "이란?",
+                            style: TextStyle(fontSize: 50),
+                          ),
+                          Container(
+                            width: 400,
+                            height: 300,
+                            child: Image(
+                              image: img,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        // width: MediaQuery.of(context).size.width,
+                        // height: MediaQuery.of(context).size.height,
+                        width: 1000,
+                        height: 1000,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        });
   }
 }
--- daitda/lib/pages/paymentPage.dart
+++ daitda/lib/pages/paymentPage.dart
@@ -8,6 +8,150 @@ class PaymentPage extends StatefulWidget {
 class _PaymentPageState extends State<PaymentPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _buildHeightSizedBox(50),
+            _buildProcessWidget(),
+            _buildBody("어떤 곳에 기부하고 싶으세요?", context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProcessWidget() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildCircle(inputColor: Color(0xff9470ac)),
+            _buildLine(inputColor: Color(0xff9470ac)),
+            _buildCircle(inputColor: Color(0xff9470ac)),
+            _buildLine(inputColor: Color(0xff9470ac)),
+            _buildCircle(inputColor: Color(0xff9470ac)),
+            _buildLine(inputColor: Color(0xffc4c4c4)),
+            _buildCircle(inputColor: Color(0xffc4c4c4)),
+            _buildLine(inputColor: Color(0xffc4c4c4)),
+            _buildCircle(inputColor: Color(0xffc4c4c4)),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildWidthSizedBox(175),
+            _buildText(inputText: "정보입력"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "카테고리 선택"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "결제"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "촬영"),
+            _buildWidthSizedBox(10),
+            _buildText(inputText: "기부완료"),
+            _buildWidthSizedBox(175),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLine({Color inputColor}) {
+    return Container(
+      height: 1,
+      width: 150,
+      color: inputColor,
+    );
+  }
+
+  Widget _buildCircle({Color inputColor}) {
+    return Container(
+      width: 22,
+      height: 22,
+      decoration: BoxDecoration(
+        color: inputColor,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+
+  Widget _buildText({String inputText}) {
+    return Container(
+      width: 80,
+      child: Text(
+        inputText,
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _buildWidthSizedBox(double _width) {
+    return SizedBox(
+      width: _width,
+    );
+  }
+
+  Widget _buildHeightSizedBox(double _height) {
+    return SizedBox(
+      height: _height,
+    );
+  }
+
+  Widget _buildBody(String inputTitle, BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: 50,
+          height: 50,
+          child: FlatButton(
+            color: Color(0xfff0f0f3),
+            onPressed: () {},
+            child: Icon(Icons.keyboard_arrow_left),
+          ),
+        ),
+        Container(
+          // color: Color(0xfff6f5f5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                inputTitle,
+                style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.start,
+              ),
+              Container(
+                color: Color(0xfff6f5f5),
+                width: 1000,
+                height: 450,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 50,
+          height: 50,
+          child: FlatButton(
+            color: Color(0xfff0f0f3),
+            onPressed: () {
+              Navigator.pushNamed(context, '/paymentPage');
+            },
+            child: Icon(Icons.keyboard_arrow_right),
+          ),
+        ),
+      ],
+    );
   }
 }
