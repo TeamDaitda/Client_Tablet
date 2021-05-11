0531a8474dc5f46e0005f7d768392d78bbc30d93
Dev-Beom <rtw_master@naver.com>
2021-05-11 16:09:09 +0900

    Update UserController
    1. Add Reset Function
    2. Add Update Logic
    3. Add Id Variable

--- daitda/lib/controller/user.dart
+++ daitda/lib/controller/user.dart
@@ -1,4 +1,3 @@
-
 import 'package:daitda/model/outputModel.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,6 +7,7 @@ class UserController extends GetxController {
 
   List<OutPut> point;
 
+  int id;
   int selectedCategoryIndex;
   String name;
   String phone;
@@ -16,6 +16,7 @@ class UserController extends GetxController {
   bool isShoot;
   bool isFinished;
 
+  int getId() => id;
   int getSelectedCategoryIndex() => selectedCategoryIndex;
   String getName() => name;
   String getPhone() => phone;
@@ -24,31 +25,55 @@ class UserController extends GetxController {
   bool getIsShoot() => isShoot;
   bool getIsFinished() => isFinished;
 
+  void setId({@required int id}) {
+    this.id = id;
+    update();
+  }
+
   void setSelectedCategoryIndex({@required int index}) {
     this.selectedCategoryIndex = index;
+    update();
   }
 
   void setName({@required String name}) {
     this.name = name;
+    update();
   }
 
   void setPhone({@required String phone}) {
     this.phone = phone;
+    update();
   }
 
   void setAffiliation({@required String affiliation}) {
     this.affiliation = affiliation;
+    update();
   }
 
   void setIsAdView({@required bool isAdView}) {
     this.isAdView = isAdView;
+    update();
   }
 
   void setIsShoot({@required bool isShoot}) {
     this.isShoot = isShoot;
+    update();
   }
 
   void setIsFinished({@required bool isFinished}) {
     this.isFinished = isFinished;
+    update();
+  }
+
+  void reset() {
+    this.point = [];
+    this.selectedCategoryIndex = -1;
+    this.name = "";
+    this.phone = "";
+    this.affiliation = "";
+    this.isAdView = false;
+    this.isShoot = false;
+    this.isFinished = false;
+    update();
   }
 }
