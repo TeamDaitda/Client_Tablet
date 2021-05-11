15b0e06136062f3cb38af6a75fe4c5deab167732
Dev-Beom <rtw_master@naver.com>
2021-05-11 16:07:58 +0900

    Update ResultPage
    1. File Upload Logic
    2. View

--- daitda/lib/pages/resultPage.dart
+++ daitda/lib/pages/resultPage.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:camera/camera.dart';
+import 'package:daitda/controller/Category.dart';
+import 'package:daitda/controller/Controllers.dart';
 import 'package:daitda/controller/imageController.dart';
 import 'package:daitda/controller/progress.dart';
 import 'package:daitda/design/colorSet.dart';
@@ -8,8 +10,11 @@ import 'package:daitda/design/designSet.dart';
 import 'package:daitda/model/outputModel.dart';
 import 'package:daitda/pages/drawPage.dart';
 import 'package:daitda/service/fileUploadApi.dart';
+import 'package:daitda/service/imageApi.dart';
 import 'package:daitda/service/imageTransApi.dart';
 import 'package:daitda/service/paintService.dart';
+import 'package:daitda/service/resultApi.dart';
+import 'package:daitda/service/userService.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
@@ -23,21 +28,32 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
+  //  Design Setting.
   final designSet = Get.put(DesignSet());
-  final progressData = Get.put(ProgressData());
   final colorSet = ColorSet();
+
+  //  Controller Setting.
+  final progressData = Get.put(ProgressData());
+  final categoryController = Get.put(Category());
+  final userController = Get.put(UserController());
 
   static double thisPageIndex;
   static double thisPageProgressIndex;
 
   List<OutPut> drawImage;
-  ImageTransApi imageAPI = ImageTransApi();
+
+  ImageTransApi _imageTransAPI = new ImageTransApi();
+  ImageApi _imageApi = new ImageApi();
+  UserApi _userApi = new UserApi();
+  FileUploadApi _fileUploadApi = new FileUploadApi();
+  ResultApi _resultApi = new ResultApi();
+
   final imageController = Get.put(ImageController());
 
   GlobalKey _globalKey = new GlobalKey();
   XFile file;
 
-  FileUploadApi _fileUploadApi = new FileUploadApi();
+  String name;
 
   Future<Uint8List> _capturePng() async {
     try {
@@ -60,14 +76,15 @@ class _ResultPageState extends State<ResultPage> {
 
   @override
   void initState() {
+    print("status data ===");
+    print(userController.name);
+
     designSet.setScreenWidthAndHeight(w: Get.size.width, h: Get.size.height);
     progressData.setData(0.2);
     file = imageController.getFile();
-
     thisPageIndex = 4;
     thisPageProgressIndex = 0.2 * (thisPageIndex + 1);
     progressData.setData(thisPageProgressIndex);
-
     super.initState();
   }
 
@@ -134,6 +151,7 @@ class _ResultPageState extends State<ResultPage> {
   Widget renderPaymentArea() {
     print("result page -----");
     print("print path + name :" + file.path + file.name);
+
     return Container(
       width: designSet.getPaymentAreaWidth(),
       height: designSet.getPaymentAreaHeight(),
@@ -142,8 +160,8 @@ class _ResultPageState extends State<ResultPage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           FutureBuilder(
-              future:
-                  imageAPI.transImage(filePath: file.path, fileName: file.name),
+              future: _imageTransAPI.transImage(
+                  filePath: file.path, fileName: file.name),
               builder: (BuildContext context, AsyncSnapshot snapshot) {
                 if (snapshot.hasData == false) {
                   return CircularProgressIndicator();
@@ -156,6 +174,18 @@ class _ResultPageState extends State<ResultPage> {
                     ),
                   );
                 } else {
+                  // 회원가입
+                  _userApi
+                      .save(
+                    affiliation: userController.getAffiliation(),
+                    category: userController.getSelectedCategoryIndex(),
+                    name: userController.getName(),
+                    phone: userController.getPhone(),
+                  )
+                      .then((value) {
+                    userController.setId(id: value);
+                  });
+
                   return Column(
                     children: [
                       Container(
@@ -165,30 +195,17 @@ class _ResultPageState extends State<ResultPage> {
                             backgroundColor: Colors.black,
                             shadowColor: Colors.white,
                           ),
-                          child: Text('포토카드 받기'),
+                          child: Text('포토카드 꾸미기'),
                           onPressed: () async {
                             final Uint8List data = await _capturePng();
-                            _fileUploadApi
-                                .upload(data: data)
-                                .then((value) => print(value));
-
-                            await Navigator.of(context).push(
-                              MaterialPageRoute<void>(
-                                builder: (BuildContext context) {
-                                  return Scaffold(
-                                    appBar: AppBar(
-                                      backgroundColor: Colors.black,
-                                    ),
-                                    body: Center(
-                                      child: Container(
-                                        color: Colors.grey,
-                                        child: Image.memory(data),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            );
+                            _fileUploadApi.upload(data: data).then((value) {
+                              _imageApi
+                                  .save(path: value, id: userController.id)
+                                  .then((_) {
+                                _resultApi.save(id: userController.id);
+                              });
+                            });
+                            Get.toNamed('/photoCardPage', arguments: data);
                           },
                         ),
                       ),
@@ -222,13 +239,6 @@ class _ResultPageState extends State<ResultPage> {
                                     ),
                                   ),
                                 ),
-                                // Align(
-                                //   alignment: Alignment.bottomCenter,
-                                //   child: Text(
-                                //     "이름이름이름이름",
-                                //     style: TextStyle(color: Colors.black),
-                                //   ),
-                                // ),
                               ],
                             ),
                           ),
